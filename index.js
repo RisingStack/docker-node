c91010cac2b8adb5838d4b137fbf2f67014e8226
Gergely Nemeth <mail@nemethgergely.com>
2016-01-03 20:01:01 +0100

    refactor(images): remove duplicated code

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const alpine = require('./alpine')
-const ubuntuTrusty = require('./ubuntu/trusty')
+const fs = require('fs')
+const path = require('path')
+const read = require('fs-readdir-recursive')
+const mkdirp = require('mkdirp')
+const Handlebars = require('handlebars')
 
-alpine.generate()
-ubuntuTrusty.generate()
+const dockerTemplates = read('.').filter(function (filePath) {
+  return filePath.includes('Dockerfile.hbs')
+})
+
+dockerTemplates.forEach((templatePath) => {
+  const dir = path.resolve(templatePath, '../')
+  const versions = require(`${dir}/versions`)
+  const source = fs.readFileSync(`${dir}/Dockerfile.hbs`, 'utf-8')
+  const template = Handlebars.compile(source)
+
+  versions.forEach((version) => {
+    const dockerFileLocation = path.join(dir, version)
+    const dockerFileContent = template({
+      VERSION: version
+    })
+
+    mkdirp.sync(dockerFileLocation)
+
+    fs.writeFileSync(`${dockerFileLocation}/Dockerfile`, dockerFileContent)
+  })
+})
