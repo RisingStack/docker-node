4db6b830631b104f45be5acfa969781865796cdf
Gergely Nemeth <mail@nemethgergely.com>
2016-02-09 12:59:13 +0100

    fix(alpine): use 3.3-1.0.5

--- alpine/versions.js
+++ alpine/versions.js
@@ -2,14 +2,14 @@ module.exports = [
   {
     base: {
       version: '3.3',
-      tag: '3.3-1.0.4'
+      tag: '3.3-1.0.5'
     },
     node: 'v4.2.6'
   },
   {
     base: {
       version: '3.3',
-      tag: '3.3-1.0.4'
+      tag: '3.3-1.0.5'
     },
     node: 'v5.5.0'
   }
