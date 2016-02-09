4e68ee1fa8797975cc3877d31c63ad2c7dcd6949
Gergely Nemeth <mail@nemethgergely.com>
2016-02-09 10:17:15 +0100

    feat(build): adding proper versioning

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const dockerTemplates = read('.').filter(function (filePath) {
   return filePath.includes('Dockerfile.hbs')
 })
 
+const circleDependencies = []
+const circleDeployments = []
+
 dockerTemplates.forEach((templatePath) => {
   const dir = path.resolve(templatePath, '../')
   const versions = require(`${dir}/versions`)
@@ -15,13 +18,28 @@ dockerTemplates.forEach((templatePath) => {
   const template = Handlebars.compile(source)
 
   versions.forEach((version) => {
-    const dockerFileLocation = path.join(dir, version)
+    const tag = `${version.base.version}-${version.node}`
+    const dockerFileLocation = path.join(dir, tag)
     const dockerFileContent = template({
-      VERSION: version
+      nodeVersion: version.node,
+      baseVersion: version.base.tag
     })
+
+    circleDependencies.push(`- version=$(node -pe "($(cat package.json)).version"); cd alpine/${tag} && docker build -t risingstack/alpine:${tag}-$version .;`)
+    circleDeployments.push(`- version=$(node -pe "($(cat package.json)).version"); cd alpine/${tag} && docker push risingstack/alpine:${tag}-$version .;`)
 
     mkdirp.sync(dockerFileLocation)
 
     fs.writeFileSync(`${dockerFileLocation}/Dockerfile`, dockerFileContent)
   })
 })
+
+const source = fs.readFileSync('./circle.hbs', 'utf-8')
+const circleTemplate = Handlebars.compile(source)
+
+const circleYml = circleTemplate({
+  deployments: circleDeployments,
+  dependencies: circleDependencies
+})
+
+fs.writeFileSync('./circle.yml', circleYml)
