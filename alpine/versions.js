8c19179fcb779c2a04aed6324e846757848e991a
Gergely Nemeth <mail@nemethgergely.com>
2016-02-13 07:40:03 +0100

    feat(dns): adding dns resolve support for kubernetes and friends
    BREAKING CHANGE: base image version changed

--- alpine/versions.js
+++ alpine/versions.js
@@ -2,14 +2,14 @@ module.exports = [
   {
     base: {
       version: '3.3',
-      tag: '3.3-1.0.5'
+      tag: '3.3-2.0.0'
     },
     node: 'v4.3.0'
   },
   {
     base: {
       version: '3.3',
-      tag: '3.3-1.0.5'
+      tag: '3.3-2.0.0'
     },
     node: 'v5.6.0'
   }
