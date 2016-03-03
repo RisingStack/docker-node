2ac4ba805257bacd77714df998ef41abeaff230f
Gergely Nemeth <mail@nemethgergely.com>
2016-03-03 06:05:02 +0100

    fix(node): version 4.3.2 and 5.7.1

--- alpine/versions.js
+++ alpine/versions.js
@@ -4,13 +4,13 @@ module.exports = [
       version: '3.3',
       tag: '3.3-2.0.0'
     },
-    node: 'v4.3.1'
+    node: 'v4.3.2'
   },
   {
     base: {
       version: '3.3',
       tag: '3.3-2.0.0'
     },
-    node: 'v5.7.0'
+    node: 'v5.7.1'
   }
 ]
