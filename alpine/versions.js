1d8a4fa650fcf10fa6a9ff3ab1a843f2a5c34517
Gergely Nemeth <mail@nemethgergely.com>
2016-04-01 08:59:14 +0200

    feat(node): adding 5.10.0 and 4.4.2

--- alpine/versions.js
+++ alpine/versions.js
@@ -4,13 +4,13 @@ module.exports = [
       version: '3.3',
       tag: '3.3-2.0.0'
     },
-    node: 'v4.4.1'
+    node: 'v4.4.2'
   },
   {
     base: {
       version: '3.3',
       tag: '3.3-2.0.0'
     },
-    node: 'v5.9.0'
+    node: 'v5.10.0'
   }
 ]
