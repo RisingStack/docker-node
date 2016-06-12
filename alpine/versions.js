a72b2e9e278a1e9fa615237a92d5d895b13a58e2
Gergely Nemeth <mail@nemethgergely.com>
2016-06-12 11:26:30 +0200

    feat(alpine): use 3.4
    BREAKING CHANGE: updated base image with new alpine

--- alpine/versions.js
+++ alpine/versions.js
@@ -1,22 +1,22 @@
   module.exports = [
   {
     base: {
-      version: '3.3',
-      tag: '3.3-2.0.0'
+      version: '3.4',
+      tag: '3.4-3.0.1'
     },
     node: 'v4.4.4'
   },
   {
     base: {
-      version: '3.3',
-      tag: '3.3-2.0.0'
+      version: '3.4',
+      tag: '3.4-3.0.1'
     },
     node: 'v5.11.1'
   },
   {
     base: {
-      version: '3.3',
-      tag: '3.3-2.0.0'
+      version: '3.4',
+      tag: '3.4-3.0.1'
     },
     node: 'v6.2.0'
   }
