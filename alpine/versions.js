16837cd57b4e643f358074792dd7d18ea6bbae5d
Gergely Nemeth <mail@nemethgergely.com>
2016-06-21 10:34:15 +0200

    fix(node): add 4.4.5 and 6.2.2

--- alpine/versions.js
+++ alpine/versions.js
@@ -4,20 +4,13 @@
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v4.4.4'
+    node: 'v4.4.5'
   },
   {
     base: {
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v5.11.1'
-  },
-  {
-    base: {
-      version: '3.4',
-      tag: '3.4-3.0.1'
-    },
-    node: 'v6.2.0'
+    node: 'v6.2.2'
   }
 ]
