d37e572c96b25bfebffc54b5f6b70c227d988ba9
Gergely Nemeth <mail@nemethgergely.com>
2016-10-28 10:48:52 +0200

    feat(version): add v7, update v6 and v4

--- alpine/versions.js
+++ alpine/versions.js
@@ -4,13 +4,20 @@
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v4.6.0'
+    node: 'v4.6.1'
   },
   {
     base: {
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v6.7.0'
+    node: 'v6.9.1'
+  },
+  {
+    base: {
+      version: '3.4',
+      tag: '3.4-3.0.1'
+    },
+    node: 'v7.0.0'
   }
 ]
