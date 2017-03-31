9ec3955e2f0def611d86e8b2f9211eae29f5a437
Gergely Nemeth <mail@nemethgergely.com>
2017-03-31 20:01:02 +0200

    feat(version): use node 6.10.1 and 7.8.0

--- alpine/versions.js
+++ alpine/versions.js
@@ -4,27 +4,27 @@
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v4.6.1'
+    node: 'v4.6.2'
   },
   {
     base: {
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v6.9.4'
+    node: 'v6.10.1'
   },
   {
     base: {
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v7.4.0'
+    node: 'v7.8.0'
   },
   {
     base: {
       version: '3.3',
       tag: '3.3-2.0.0'
     },
-    node: 'v6.9.4'
+    node: 'v6.10.1'
   }
 ]
