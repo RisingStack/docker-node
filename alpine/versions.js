e3e8d7dda17063690f5a258a95b721e337ecb894
Gergely Nemeth <mail@nemethgergely.com>
2017-07-12 07:27:53 +0100

    fix(node): add security fixes

--- alpine/versions.js
+++ alpine/versions.js
@@ -4,34 +4,34 @@
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v4.8.2'
+    node: 'v4.8.4'
   },
   {
     base: {
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v6.10.3'
+    node: 'v6.11.1'
   },
   {
     base: {
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v7.9.0'
+    node: 'v7.10.1'
   },
     {
     base: {
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v8.0.0'
+    node: 'v8.1.4'
   },
   {
     base: {
       version: '3.3',
       tag: '3.3-2.0.0'
     },
-    node: 'v6.10.3'
+    node: 'v6.11.1'
   }
 ]
