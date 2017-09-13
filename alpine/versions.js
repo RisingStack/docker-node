ff51a6b0d461586314a6c9090037f4852a403db5
Gergely Nemeth <mail@nemethgergely.com>
2017-09-13 14:00:41 +0200

    feat(version): add node 8.5

--- alpine/versions.js
+++ alpine/versions.js
@@ -4,34 +4,6 @@
       version: '3.4',
       tag: '3.4-3.0.1'
     },
-    node: 'v4.8.4'
-  },
-  {
-    base: {
-      version: '3.4',
-      tag: '3.4-3.0.1'
-    },
-    node: 'v6.11.2'
-  },
-  {
-    base: {
-      version: '3.4',
-      tag: '3.4-3.0.1'
-    },
-    node: 'v7.10.1'
-  },
-    {
-    base: {
-      version: '3.4',
-      tag: '3.4-3.0.1'
-    },
-    node: 'v8.4.0'
-  },
-  {
-    base: {
-      version: '3.3',
-      tag: '3.3-2.0.0'
-    },
-    node: 'v6.11.2'
+    node: 'v8.5.0'
   }
 ]
